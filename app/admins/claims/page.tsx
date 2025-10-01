f27d440c0945e5eb734955da34257bd5fd7bba76
Vicky <152793789+VIKNESWARAN-VALACHAN@users.noreply.github.com>
2025-10-01 14:39:01 +0800

    Update page.tsx
    update UI for export button

--- app/admins/claims/page.tsx
+++ app/admins/claims/page.tsx
@@ -1971,14 +1971,16 @@ export default function AdminClaimListPage() {
           <div className="flex gap-2">
             <button 
               onClick={() => setIsReportModalOpen(true)}
-              className="btn btn-outline btn-primary btn-sm"
+              className="btn btn-sm sm:btn-md w-full sm:w-auto flex items-center gap-2 btn-primary"//className="btn btn-outline btn-primary btn-sm"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
               </svg>
               Export Report
             </button>
-            <button className="btn btn-primary btn-sm" onClick={openCreateModal}>
+            <button 
+              className="btn btn-sm sm:btn-md w-full sm:w-auto flex items-center gap-2 btn-primary"//className="btn btn-primary btn-sm" 
+              onClick={openCreateModal}>
               <Plus className="w-4 h-4 mr-1" /> New Claim (On Behalf)
             </button>
           </div>
